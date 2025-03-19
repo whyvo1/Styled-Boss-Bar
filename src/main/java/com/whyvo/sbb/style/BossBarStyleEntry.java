0a725832214efa31eca93f9c077b46c21ea4648d
w4017 <w401789064@outlook.com>
2025-03-19 21:55:12 +0800

    updated 250319

--- src/main/java/com/whyvo/sbb/style/BossBarStyleEntry.java
+++ src/main/java/com/whyvo/sbb/style/BossBarStyleEntry.java
@@ -2,5 +2,20 @@ package com.whyvo.sbb.style;
 
 import net.minecraft.util.Identifier;
 
-public record BossBarStyleEntry<T extends BossBarStyle>(Identifier id, T value) {
+public class BossBarStyleEntry<T extends BossBarStyle> {
+    private final Identifier id;
+    private final T value;
+
+    BossBarStyleEntry(Identifier id, T value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public Identifier id() {
+        return this.id;
+    }
+
+    public T value() {
+        return this.value;
+    }
 }
