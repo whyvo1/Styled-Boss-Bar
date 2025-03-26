37f835abfce4b5e1a65f798f0718ec16b99a2ccb
w4017 <w401789064@outlook.com>
2025-03-26 19:12:17 +0800

    jade compat

--- src/main/java/com/whyvo/sbb/injectface/BossBarHudInjected.java
+++ src/main/java/com/whyvo/sbb/injectface/BossBarHudInjected.java
@@ -1,9 +1,21 @@
 package com.whyvo.sbb.injectface;
 
 
+import com.whyvo.sbb.bossbar.ClientStyledBossBar;
 import com.whyvo.sbb.network.StyledBossBarS2CPayload;
+
+import java.util.Map;
+import java.util.UUID;
 
 public interface BossBarHudInjected {
     default void styledBossBarAPI$onStyledBossBar(StyledBossBarS2CPayload packet) {
+    }
+
+    default Map<UUID, ClientStyledBossBar> styledBossBarAPI$getStyledBossBars() {
+        return null;
+    }
+
+    default int styledBossBarAPI$getCachedHeight() {
+        return 0;
     }
 }
